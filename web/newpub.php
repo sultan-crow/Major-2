592ded59772c59150b18793e0fae566bf99e4088
sultan_mirza <ketank90@gmail.com>
2015-05-05 12:41:52 +0530

    Registration form added

---
+++ web/newpub.php
@@ -0,0 +1,56 @@
+<?php
+date_default_timezone_set('Asia/Calcutta');
+
+$action = isset($_POST["action"]) ? $_POST["action"] : "";
+if (empty($action)) {
+	// Send back the contact form HTML
+	$output = "<div style='display:none'>
+	<div class='contact-top'></div>
+	<div class='contact-content'>
+		<h1 class='contact-title'>Post a new thread:</h1>
+		<div class='contact-loading' style='display:none'></div>
+		<div class='contact-message' style='display:none'></div>
+		<form action='#' style='display:none'>
+			<label for='contact-title'>*Title:</label>
+			<input type='text' id='contact-title' class='contact-input' name='title' tabindex='1001' />"
+			;
+	
+
+	$output .= "
+			<label for='contact-text'>*Text:</label>
+			<textarea id='contact-text' class='contact-input' name='text' cols='40' rows='4' tabindex='1004'></textarea>
+			<br/>";
+
+	
+
+	$output .= "
+			<label>&nbsp;</label>
+			<button type='submit' class='contact-send contact-button' tabindex='1006'>Send</button>
+			<button type='submit' class='contact-cancel contact-button simplemodal-close' tabindex='1007'>Cancel</button>
+			<br/>
+					</form>
+</div>";
+	echo $output;
+}
+else if ($action == "send") {
+
+	include "../connection.php";
+	session_start();
+	$user=$_SESSION['user'];
+	$class=$_SESSION['class'];
+	// Send the email
+	$title = isset($_POST["title"]) ? $_POST["title"] : "";
+	$text = isset($_POST["text"]) ? $_POST["text"] : "";
+		
+	$date = date('Y-m-d', time());
+	$time = date('H:i:s', time());
+	
+	//TODO: Insert to database
+	
+	$query = "INSERT INTO posts (posted_by,class,post_title,post_text,time,date) VALUES ('$user','$class','$title','$text','$time','$date')";
+	mysql_query($query, $con) or die (mysql_error());
+	mysql_close($con);
+}
+
+
+?>
---
+++ web/students/js/contact.js
@@ -0,0 +1,151 @@
+
+jQuery(function ($) {
+	var contact = {
+		message: null,
+		init: function () {
+			$('#newpost').click(function (e) {
+				e.preventDefault();
+
+				// load the contact form using ajax
+				$.get("../contact.php", function(data){
+					// create a modal dialog with the data
+					$(data).modal({
+						closeHTML: "<a href='#' title='Close' class='modal-close'>x</a>",
+						position: ["15%",],
+						overlayId: 'contact-overlay',
+						containerId: 'contact-container',
+						onOpen: contact.open,
+						onShow: contact.show,
+						onClose: contact.close
+					});
+				});
+			});
+		},
+		open: function (dialog) {
+			// dynamically determine height
+			var h = 280;
+			if ($('#contact-subject').length) {
+				h += 26;
+			}
+			if ($('#contact-cc').length) {
+				h += 22;
+			}
+
+			var title = $('#contact-container .contact-title').html();
+			$('#contact-container .contact-title').html('Loading...');
+			dialog.overlay.fadeIn(200, function () {
+				dialog.container.fadeIn(200, function () {
+					dialog.data.fadeIn(200, function () {
+						$('#contact-container .contact-content').animate({
+							height: h
+						}, function () {
+							$('#contact-container .contact-title').html(title);
+							$('#contact-container form').fadeIn(200, function () {
+								$('#contact-container #contact-name').focus();
+
+							});
+						});
+					});
+				});
+			});
+		},
+		show: function (dialog) {
+			$('#contact-container .contact-send').click(function (e) {
+				e.preventDefault();
+				// validate form
+				if (contact.validate()) {
+					var msg = $('#contact-container .contact-message');
+					msg.fadeOut(function () {
+						msg.removeClass('contact-error').empty();
+					});
+					$('#contact-container .contact-title').html('Sending...');
+					$('#contact-container form').fadeOut(200);
+					$('#contact-container .contact-content').animate({
+						height: '80px'
+					}, function () {
+						$('#contact-container .contact-loading').fadeIn(200, function () {
+							$.ajax({
+								url: '../contact.php',
+								data: $('#contact-container form').serialize() + '&action=send',
+								type: 'post',
+								cache: false,
+								dataType: 'html',
+								success: function (data) {
+									$('#contact-container .contact-loading').fadeOut(200, function () {
+										$('#contact-container .contact-title').html('Thank you!<br>Your Post has been submitted successfully');
+										msg.html(data).fadeIn(200);
+										console.log("Hello" + data);
+									});
+								},
+								error: contact.error
+							});
+						});
+					});
+				}
+				else {
+					if ($('#contact-container .contact-message:visible').length > 0) {
+						var msg = $('#contact-container .contact-message div');
+						msg.fadeOut(200, function () {
+							msg.empty();
+							contact.showError();
+							msg.fadeIn(200);
+						});
+					}
+					else {
+						$('#contact-container .contact-message').animate({
+							height: '30px'
+						}, contact.showError);
+					}
+					
+				}
+			});
+		},
+		close: function (dialog) {
+			$('#contact-container .contact-message').fadeOut();
+			$('#contact-container .contact-title').html('Goodbye...');
+			$('#contact-container form').fadeOut(200);
+			$('#contact-container .contact-content').animate({
+				height: 40
+			}, function () {
+				dialog.data.fadeOut(200, function () {
+					dialog.container.fadeOut(200, function () {
+						dialog.overlay.fadeOut(200, function () {
+							$.modal.close();
+						});
+					});
+				});
+			});
+		},
+		error: function (xhr) {
+			alert(xhr.statusText);
+		},
+		validate: function () {
+			
+			
+contact.message="";
+			if (!$('#contact-container #contact-text').val()) {
+				contact.message = 'Message is required.';
+			}
+			
+			if (!$('#contact-container #contact-title').val()) {
+				contact.message = 'Title is required.';
+			}
+
+			if (contact.message.length > 0) {
+				return false;
+			}
+			else {
+				return true;
+			}
+		},
+		
+		showError: function () {
+			$('#contact-container .contact-message')
+				.html($('<div class="contact-error"></div>').append(contact.message))
+				.fadeIn(200);
+		}
+	};
+
+	contact.init();
+
+});
